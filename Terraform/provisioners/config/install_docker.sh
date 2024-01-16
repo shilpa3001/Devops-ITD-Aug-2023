b60b90991ef1b8e2e53b60077896174f052e6b43
Harsha Jain <jaintpharsha@gmail.com>
2024-01-16 09:09:06 +0530

    update 16-January-24 09:09AM

--- Terraform/provisioners/config/install_docker.sh
+++ Terraform/provisioners/config/install_docker.sh
@@ -2,6 +2,7 @@
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
+[[ -f "/etc/apt/keyrings/docker.gpg" ]] && sudo rm -f /etc/apt/keyrings/docker.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
